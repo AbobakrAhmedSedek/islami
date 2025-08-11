1a9b712b39ed9ad92d9a6e445316cabaf1302a08
AbobakrAhmedSedek <123852zza@gmail.com>
2025-08-11 03:23:28 +0300

    + Change external application icon

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -7,20 +7,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Screen'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'Welcome to the Home Screen',
-            style: TextStyle(fontSize: 24, color: Colors.black),
-          ),
-        ),
-      ),
-    );
+    return
   }
 }
