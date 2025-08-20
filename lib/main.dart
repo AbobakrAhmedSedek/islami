61ffdb10907564fd864e4a52d72514120efcda4e
AbobakrAhmedSedek <123852zza@gmail.com>
2025-08-20 11:21:13 +0300

    + add a Logic search field

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami/ui/home/home_screen.dart';
 import 'package:islami/ui/home/tabs/quran/Display_sura_as_connected_verses/sura_details_connected_verses_screen.dart';
+import 'package:islami/ui/home/tabs/quran/Display_sura_each_verse_per_line/sura_details_verse_per_line_screen.dart';
 import 'package:islami/ui/onboarding_screen/onboarding_screen.dart';
 import 'package:islami/utils/appTheme.dart';
 
@@ -21,7 +22,8 @@ class MyApp extends StatelessWidget {
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetailsConnectedVersesScreen.routeName:
             (context) => SuraDetailsConnectedVersesScreen(),
-        // SuraDetailsVersePerLineScreen.routeName: (context) => SuraDetailsVersePerLineScreen(),
+        SuraDetailsVersePerLineScreen.routeName:
+            (context) => SuraDetailsVersePerLineScreen(),
         '/': (context) => const OnboardingScreen(),
       },
       darkTheme: AppTheme.darkTheme,
